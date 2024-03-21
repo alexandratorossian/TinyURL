8e9c608fc0f1fae914004aee774282f1595510a3
alexandratorossian <alexandra.torossian@outlook.com>
2024-03-21 15:10:52 +0100

    added baseUrl in app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,22 @@
+const baseUrl = 'http://127.0.0.1:8000'; // URL de base du serveur, modifier cette variable en fonction de votre configuration
+
+function redirectToLongUrl(tabId) {
+    console.log("Envoi de la requête pour récupérer l'URL longue de", tabId);
+    fetch(`${baseUrl}/${tabId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur lors de la récupération de l\'URL longue');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("URL longue récupérée :", data.long_url);
+            window.location.href = data.long_url;
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération de l\'URL longue:', error);
+        });
+}
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -22,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function redirectToLongUrl(tabId) {
     console.log("Envoi de la requête pour récupérer l'URL longue de", tabId);
-    fetch(`api/long-url/${tabId}`)
+    fetch(`${baseUrl}/api/long-url/${tabId}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Erreur lors de la récupération de l\'URL longue');
